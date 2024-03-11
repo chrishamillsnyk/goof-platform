d3654abfa8c28de3b089f3cf78a10ae06089808e
chrishamillsnyk <88782898+chrishamillsnyk@users.noreply.github.com>
2024-03-11 11:20:50 +0000

    Update app.js

--- Code/app.js
+++ Code/app.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2014-2020 Bjoern Kimminich.
  * SPDX-License-Identifier: MIT
  */
-
+ 
 require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
